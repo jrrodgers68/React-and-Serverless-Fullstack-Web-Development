8102154a3e981ed2fd91b76e14440485c616225e
John Rodgers <jrrodgers68@gmail.com>
2020-05-28 00:19:46 -0400

    styled the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  StyledNavbar,
+  StyledNavBrand,
+  StyledNavItems,
+  StyledLink,
+} from '../styled/Navbar';
+import { Accent } from '../styled/Random';
 
 export default function Navbar() {
-    return (
-        <nav>
-            <div>
-                <Link to="/">
-                    Learn.Build.<span>Type.</span>
-                </Link>
-            </div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/highScores">High Scores</Link>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <StyledNavbar>
+      <StyledNavBrand>
+        <Link to="/">
+          Learn.Build.<Accent>Type.</Accent>
+        </Link>
+      </StyledNavBrand>
+      <StyledNavItems>
+        <li>
+          <StyledLink to="/">Home</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/highScores">High Scores</StyledLink>
+        </li>
+      </StyledNavItems>
+    </StyledNavbar>
+  );
 }
