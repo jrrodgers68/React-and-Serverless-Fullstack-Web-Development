f510f59760d615b63c9a9039a146116d527e9f2c
John Rodgers <jrrodgers68@gmail.com>
2020-05-30 19:51:31 -0400

    styled game component

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {
+  StyledGame,
+  StyledScore,
+  StyledTimer,
+  StyledCharacter,
+} from '../styled/Game';
+import { Strong } from '../styled/Random';
 
 export default function Game() {
-    return (
-        <div>
-           <h1>Game</h1> 
-        </div>
-    );
+  return (
+    <StyledGame>
+      <StyledScore>
+        Score:<Strong>0</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Time: <Strong>00: 000</Strong>
+      </StyledTimer>
+    </StyledGame>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import CTA from '../styled/CTA';
+import { Accent, StyledTitle } from '../styled/Random';
 
-export default function Home()  {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+export default function Home() {
+  return (
+    <div>
+      <StyledTitle>Ready to type?</StyledTitle>
+      <CTA to="/game">
+        Click or type <Accent>s</Accent> to start playing!
+      </CTA>
+    </div>
+  );
 }
